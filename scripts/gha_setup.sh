41ab74e234d8a472a62839dffc73170445d0d90d
shushank <shushankpawar664@gmail.com>
2025-01-23 17:39:31 +0530

    sorry :( i am unemployed so i tried to modify this code.... :))

--- scripts/gha_setup.sh
+++ scripts/gha_setup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Configure SBT options
-[[ -n "${BEAM_RUNNERS}" ]]                   && echo "-DbeamRunners=$BEAM_RUNNERS"                                >> .sbtopts
-[[ -n "${GOOGLE_CLOUD_PROJECT}" ]]           && echo "-Dbigquery.project=$GOOGLE_CLOUD_PROJECT"                   >> .sbtopts
-[[ -n "${GOOGLE_APPLICATION_CREDENTIALS}" ]] && echo "-Dbigquery.secret=$GOOGLE_APPLICATION_CREDENTIALS"          >> .sbtopts
-[[ -n "${BQ_CONNECT_TIMEOUT}" ]]             && echo "-Dbigquery.connect_timeout=$BQ_CONNECT_TIMEOUT"             >> .sbtopts
-[[ -n "${BQ_READ_TIMEOUT}" ]]                && echo "-Dbigquery.read_timeout=$BQ_READ_TIMEOUT"                   >> .sbtopts
-[[ -n "${CLOUDSQL_SQLSERVER_PASSWORD}" ]]    && echo "-Dcloudsql.sqlserver.password=$CLOUDSQL_SQLSERVER_PASSWORD" >> .sbtopts
+# Function to configure SBT options based on environment variables
+configure_sbt_option () {
+  local env_var_name=$1
+  local sbt_key=$2
+  local env_var_value=${!env_var_name}
+
+  if [[ -n "$env_var_value" ]]; then
+    echo "-D$sbt_key=$env_var_value" >> .sbtopts
+  fi
+}
+
+# List of environment variables and corresponding SBT keys
+declare -A sbt_options=(
+  ["BEAM_RUNNERS"]="beamRunners"
+  ["GOOGLE_CLOUD_PROJECT"]="bigquery.project"
+  ["GOOGLE_APPLICATION_CREDENTIALS"]="bigquery.secret"
+  ["BQ_CONNECT_TIMEOUT"]="bigquery.connect_timeout"
+  ["BQ_READ_TIMEOUT"]="bigquery.read_timeout"
+  ["CLOUDSQL_SQLSERVER_PASSWORD"]="cloudsql.sqlserver.password"
+)
+
+# Configure each SBT option
+for env_var in "${!sbt_options[@]}"; do
+  configure_sbt_option "$env_var" "${sbt_options[$env_var]}"
+done
